12a5fa17cdc87dd8b6db11f15531fb9db7ba49cb
Adam Akkari <adam.akkari@epitech.eu>
2026-01-09 23:57:16 +0100

    Fucking finally made the categories properly display on french word list view thing

--- dictionary_app/darijapp-dictionary-frontend/src/components/French/FrenchExpressionsList.tsx
+++ dictionary_app/darijapp-dictionary-frontend/src/components/French/FrenchExpressionsList.tsx
@@ -4,6 +4,7 @@ import type { FrenchWithTranslations } from '../../models/FrenchWithTranslations
 import ArabicWord from '../Arabic/ArabicWord';
 import { removeAccents } from '../../helpers/SearchHelper';
 import WordDisplay from '../WordDisplay';
+import type { Category } from '../../models/Category';
 
 interface FrenchExpressionsListProps {
     selectedItem: FrenchWithTranslations | null,
@@ -15,6 +16,7 @@ interface FrenchExpressionsListProps {
 function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, filter }: FrenchExpressionsListProps) {
     const [frenchExpressions, setFrenchExpressions] = useState<FrenchWithTranslations[]>([]);
     const [filteredFrenchExpressions, setFilteredFrenchExpressions] = useState<FrenchWithTranslations[]>([]);
+    const [categories, setCategories] = useState<{ [key: number]: Category[] }>({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -25,6 +27,10 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
     useEffect(() => {
         updateFilter();
     }, [filter]);
+
+    useEffect(() => {
+        updateCategories();
+    }, [filteredFrenchExpressions]);
 
     const fetchData = async () => {
         try {
@@ -44,7 +50,7 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
         }
     };
 
-    const updateFilter = () => {
+    const updateFilter = async () => {
         if (!filter || filter.trim() === "") {
             setFilteredFrenchExpressions(frenchExpressions);
         }
@@ -52,6 +58,21 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
             setFilteredFrenchExpressions(frenchExpressions.filter(item => removeAccents(item.expression).toLowerCase().includes(removeAccents(filter).toLowerCase())))
         }
     }
+
+    const updateCategories = async () => {
+        // Fetch all categories in parallel, returning an array of [id, categories] pairs:
+        const entries = await Promise.all(
+            filteredFrenchExpressions.map(async c => {
+                const cats = await getCategories(c.id);
+                return [c.id, cats] as [number, Category[]];
+            })
+        );
+
+        // Turn the array of pairs into an object/dictionary:
+        const categoriesDictionary = Object.fromEntries(entries);
+        // Update state
+        setCategories(categoriesDictionary);
+    };
 
     const deleteWord = async () => {
         if (selectedItem == null) {
@@ -79,6 +100,39 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
         }
     }
 
+    const getCategories = async (frenchId: number): Promise<Category[]> => {
+        try {
+            const response = await fetch('api/expressions/french/' + frenchId + '/categories');
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch categories');
+            }
+
+            console.log("WESH???")
+            return await response.json();
+        } catch (err) {
+            setError(err instanceof Error ? err.message : 'An error occurred');
+            return [];
+        }
+    }
+
+    const displayCategories = (frenchId: number) => {
+        if (categories.hasOwnProperty(frenchId)) {
+            return (<>
+                {categories[frenchId].map((category) => (
+                    <div key={category.id}>
+                        <WordDisplay isSelected={false}>
+                            {category.category_name}
+                        </WordDisplay>
+                    </div>
+                ))}
+            </>);
+        }
+        else {
+            return (<></>);
+        }
+    }
+
     if (loading)
         return <div>Loading...</div>;
     if (error)
@@ -90,6 +144,7 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
                 <div className='expression-table-header'>
                     <div className='expression-table-cell'>French</div>
                     <div className='expression-table-cell'>Arabic</div>
+                    <div className='expression-table-cell'>Category</div>
                     <div className='expression-table-cell-tool'></div>
                 </div>
                 {filteredFrenchExpressions.map((item) => (
@@ -106,6 +161,9 @@ function FrenchExpressionsList({ selectedItem, setSelectedItem, editCallback, fi
                                     </WordDisplay>
                                 </div>
                             ))}
+                        </div>
+                        <div className='expression-table-cell'>
+                            {displayCategories(item.id)}
                         </div>
                         <div className='expression-table-cell-tool'>
                             <button disabled={selectedItem?.id != item.id} onClick={() => editCallback()}>Edit</button>
