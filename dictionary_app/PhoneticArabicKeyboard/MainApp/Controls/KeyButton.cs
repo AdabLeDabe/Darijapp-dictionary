821c837ec7b7c20d7e81345908765f3c1b8b8401
Adam Akkari <adam.akkari@epitech.eu>
2026-01-12 13:44:49 +0100

    Improved display of keyboard by adding arabic letters

--- dictionary_app/PhoneticArabicKeyboard/MainApp/Controls/KeyButton.cs
+++ dictionary_app/PhoneticArabicKeyboard/MainApp/Controls/KeyButton.cs
@@ -1,6 +1,7 @@
 ﻿using MainApp.Helper;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,17 +17,50 @@ namespace MainApp.Controls
                 typeof(KeyButton),
                 new PropertyMetadata(default(char), OnKeyCharChanged));
 
+        public static readonly DependencyProperty ArabicCharProperty =
+            DependencyProperty.Register(
+                "ArabicChar",
+                typeof(char),
+                typeof(KeyButton),
+                new PropertyMetadata(default(char), OnArabicCharChanged));
+
         public char KeyChar
         {
             get => (char)GetValue(KeyCharProperty);
             set => SetValue(KeyCharProperty, value);
         }
 
+        public char ArabicChar
+        {
+            get => (char)GetValue(ArabicCharProperty);
+            set => SetValue(ArabicCharProperty, value);
+        }
+
         private static void OnKeyCharChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is KeyButton keyButton)
             {
+                UpdateKeyButtonDisplay(keyButton);
+            }
+        }
+
+        private static void OnArabicCharChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is KeyButton keyButton)
+            {
+                UpdateKeyButtonDisplay(keyButton);
+            }
+        }
+
+        private static void UpdateKeyButtonDisplay(KeyButton keyButton)
+        {
+            if (keyButton.ArabicChar == 0)
+            {
                 keyButton.Content = keyButton.KeyChar.ToString();
+            }
+            else
+            {
+                keyButton.Content = keyButton.KeyChar.ToString() + Environment.NewLine + keyButton.ArabicChar.ToString();
             }
         }
 
